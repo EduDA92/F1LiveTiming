5791761b6c80a8b592fb11bc6068a1ca0ccc1b5f
Edu <ediago92@gmail.com>
2024-06-11 16:18:03 +0200

    Update Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockwebserver)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
