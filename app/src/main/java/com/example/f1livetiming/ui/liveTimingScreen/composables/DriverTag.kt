462f35b7b7526987b6f5626c6813519dd43855bd
Edu <ediago92@gmail.com>
2024-06-20 16:32:17 +0200

    import error

--- app/src/main/java/com/example/f1livetiming/ui/liveTimingScreen/composables/DriverTag.kt
+++ app/src/main/java/com/example/f1livetiming/ui/liveTimingScreen/composables/DriverTag.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.f1livetiming.ui.theme.RedBull
 
 @Composable
 fun DriverTag(
