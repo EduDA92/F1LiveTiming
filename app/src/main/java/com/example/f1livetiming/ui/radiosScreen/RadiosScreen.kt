fed0d3cd9c4d284269b87ed932033fdcfda15eed
Edu <ediago92@gmail.com>
2024-08-07 14:17:13 +0200

    radioScreen starting ver

---
+++ app/src/main/java/com/example/f1livetiming/ui/radiosScreen/RadiosScreen.kt
@@ -0,0 +1,36 @@
+package com.example.f1livetiming.ui.radiosScreen
+
+import android.provider.MediaStore.Audio.Radio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RadiosScrenRoute(
+    modifier: Modifier = Modifier
+){
+
+    RadiosScreen(
+        modifier = modifier
+    )
+
+}
+
+@Composable
+fun RadiosScreen(
+    modifier: Modifier = Modifier
+){
+
+        Text("Radios Screen", modifier = modifier.fillMaxSize())
+
+}
+
+@Preview
+@Composable
+fun RadiosScreenPreview(){
+
+    RadiosScreen()
+
+}
