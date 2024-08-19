d2f72186a33302119c709912fa26e19c14031751
Edu <ediago92@gmail.com>
2024-08-19 13:02:29 +0200

    TeamsRadio Mapper

---
+++ app/src/main/java/com/example/f1livetiming/data/mapper/TeamsRadioMapper.kt
@@ -0,0 +1,13 @@
+package com.example.f1livetiming.data.mapper
+
+import com.example.f1livetiming.data.network.model.TeamRadioDTO
+import com.example.f1livetiming.ui.model.TeamRadio
+
+fun TeamRadioDTO.asUIModel(): TeamRadio {
+    return TeamRadio(
+        driverNumber = driverNumber,
+        recordingUrl = recordingUrl
+    )
+}
+
+fun List<TeamRadioDTO>.asUIModel() = map { it.asUIModel() }
